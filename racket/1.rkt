ebf03d4199cf4532ae8488c065271e8593289513
Kenny Shen <kenny@machinesung.com>
2018-12-28 03:06:13 +0800

    Racket #1, untyped -> typed

--- racket/1.rkt
+++ racket/1.rkt
@@ -1,31 +1,31 @@
-#lang racket
+#lang typed/racket
 
-(require rackunit)
+(require typed/rackunit)
 
-; Number -> Number
-; takes a n and checks if it is a multiple of k
+; Takes a n and checks if it is a multiple of k
 ; given: n=9, k=3, expect: t
 ; given: n=9, k=2, expect: f
+(: is-multiple-of (-> Integer Integer Boolean))
 (define (is-multiple-of n k)
   (zero? (modulo n k)))
 
 (check-equal? (is-multiple-of 9 3) #t)
 (check-equal? (is-multiple-of 9 2) #f)
 
-; List of Number -> Number
-; takes a list of numbers and return the sum
+; Takes a list of numbers and return the sum
 ; given: '(1 2 3), expect: 6
+(: sumList (-> (Listof Integer) Integer))
 (define (sumList xs)
-  (foldl (lambda (v i) (+ v i)) 0 xs))
+  (foldl (lambda ([v : Integer] [i : Integer]) (+ v i)) 0 xs))
 
 (check-equal? (sumList '(1 2 3)) 6)
 
-; List of Number -> List of Number
-; takes a list of numbers and return those that are
+; Takes a list of numbers and return those that are
 ; a multiple of 3 or 5
 ; given: '(1 2 3 4 5 6), expect: '(3 5 6)
+(: filter-multiples-of-three-or-five (-> (Listof Integer) (Listof Integer)))
 (define (filter-multiples-of-three-or-five xs)
-  (filter (lambda (i) (or (is-multiple-of i 3) (is-multiple-of i 5))) xs))
+  (filter (lambda ([i : Integer]) (or (is-multiple-of i 3) (is-multiple-of i 5))) xs))
 
 (check-equal? (filter-multiples-of-three-or-five '(1 2 3 4 5 6)) '(3 5 6))
 
